c52d9ad6ae5459764115f610aefb4d96ee5012fe
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-25 16:44:54 -0500

    Operators -> exercise 3

---
+++ src/main/java/com/paridile/operators/Test.java
@@ -0,0 +1,44 @@
+package com.paridile.operators;
+
+//Given the following program, overrides the equals method to get the output:
+//false
+//true
+
+class Person {
+}
+
+class Student {
+
+	int id;
+
+	Student(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Student) {
+			Student s = (Student) obj;
+			return s.id == id;
+		}
+		return false;
+	}
+
+}
+
+public class Test {
+	public static void main(String[] args) {
+
+		Person p = new Person();
+
+		Student s1 = new Student(1001);
+
+		Student s2 = new Student(1001);
+
+		System.out.println(s1.equals(p));
+
+		System.out.println(s1.equals(s2));
+
+	}
+
+}
