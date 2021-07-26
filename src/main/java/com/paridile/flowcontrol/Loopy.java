d2a3acf75d4b43e2bfd7255ec8fe11f507964aa9
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-26 16:27:30 -0500

    FlowControl exercises

---
+++ src/main/java/com/paridile/flowcontrol/Loopy.java
@@ -0,0 +1,12 @@
+package com.paridile.flowcontrol;
+
+class Loopy {
+	public static void main(String[] args) {
+		int[] x = { 7, 6, 5, 4, 3, 2, 1 };
+
+		for (int y : x) {
+			System.out.print(y + " ");
+		}
+	}
+
+}
