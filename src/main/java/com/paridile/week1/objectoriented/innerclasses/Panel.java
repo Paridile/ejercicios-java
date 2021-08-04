71da7306d089e652aed63656a11034407052d17d
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-08-04 15:48:33 -0500

    Refactored Panel

---
+++ src/main/java/com/paridile/week1/objectoriented/innerclasses/Panel.java
@@ -0,0 +1,17 @@
+package com.paridile.week1.objectoriented.innerclasses;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Panel {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Prueba clase interna");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        JPanel panel = new JPanel();
+        panel.add(new Button("Haz click"));
+        frame.setContentPane(panel);
+        frame.setVisible(true);
+    }
+
+}
