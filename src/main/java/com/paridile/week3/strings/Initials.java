6ffff2ad19a2f74e57bed2b889643fe4d4c2b9e7
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-08-09 10:00:49 -0500

    Refactored initials using stringbuilder

--- src/main/java/com/paridile/week3/strings/Initials.java
+++ src/main/java/com/paridile/week3/strings/Initials.java
@@ -10,11 +10,10 @@ public class Initials {
 
 	public static String initials(String name) {
 		String[] words = name.trim().split("\\s+");
-		String initials = "";
-		for (int i = 0; i < words.length; i++) {
-			initials += words[i].charAt(0);
+		StringBuilder initials = new StringBuilder();
+		for (String word : words) {
+			initials.append(word.charAt(0));
 		}
-		initials = initials.toUpperCase();
-		return initials;
+		return initials.toString().toUpperCase();
 	}
 }
